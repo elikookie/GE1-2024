598cd18e204cbe131b4b2dfee3d2439f2318583b
Elitsa Koleva <53860330+elikookie@users.noreply.github.com>
2024-09-27 17:22:48 +0100

    made rand colored bricks

---
+++ games-engines-2024/wall.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var brick_scene: PackedScene
+@export var rows = 10
+@export var cols = 10
+
+func _ready():
+	for row in range(rows):
+		for col in range(cols):
+			var brick = brick_scene.instantiate()
+			var mesh_instance = brick.get_node("MeshInstance3D")
+			var material = StandardMaterial3D.new()
+			
+			var r = randf()
+			var g = randf()
+			var b = randf()
+			var random_color = Color(r, g, b, 1)
+			material.albedo_color = random_color
+			mesh_instance.material_override = material
+			
+			var pos = Vector3(col, row , 0)
+						
+			brick.position = pos
+			
+			add_child(brick)
+			
+
+
+func _process(delta):
+	pass
